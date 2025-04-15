0d4fe3e600861b234493773d2a3c6f95b0a74579
2534Nicolle <162452766+2534Nicolle@users.noreply.github.com>
2025-04-15 12:27:15 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function alternarVisibilidade() {
     const div = document.getElementById("minhaDiv");
     if (div.style.display === "none") {
-      div.style.display = "block"; // Mostra a div
+      div.style.display = "block"; 
     } else {
-      div.style.display = "none"; // Esconde a div
+      div.style.display = "none"; 
     }
   }
